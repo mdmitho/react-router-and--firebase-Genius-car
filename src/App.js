4eeee35c10552d1bd2707c21e84153f9310154e8
Hossen <mdmitho199911@gmail.com>
2022-04-13 04:35:06 +0600

    dynamic route params

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Header></Header>
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
       <Route path='/about' element={<About></About>}></Route>
     </Routes>
     <Footer></Footer>
